e4025bbfa6aea5f8b775b73c101eb2daf93a946b
baoacheng <caifeng_muc@163.com>
2023-10-11 23:14:19 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="increment">Click me</button>
+  <span>{{ cnt }}</span>
+  <button @click="clear">Clear</button>
 </template>
 
 <script>
@@ -8,8 +11,26 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      cnt: 0
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.cnt = this.$store.state.count;
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+      console.log(this.$store.state.count);
+      this.cnt = this.$store.state.count;
+    },
+    clear() {
+      this.$store.state.count = 0;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 创建一个新的 store 实例
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+const app = createApp(App);
+app.use(store);
+app.mount('#app');
